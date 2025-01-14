ff8bd81f0809206333dfa02417c4ef2de560e1a3
Eboh Samuel <ebohsamuel100@yahoo.com>
2025-01-14 16:46:56 +0100

    updating and creating new files

--- transport_app/main.py
+++ transport_app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, Request
 from transport_app.user_authentication import template
-from transport_app.router import login, welcome, user_registration,logout,update_user, driver_registration
+from transport_app.router import login, welcome, user_registration, logout, update_user, driver_registration
 from transport_app.router import trip_registration, trip_update, expense_registration, update_expense
-from transport_app.router import update_driver
+from transport_app.router import update_driver, trip_report, expense_report
 
 app = FastAPI()
 
@@ -17,6 +17,8 @@ app.include_router(trip_update.router)
 app.include_router(expense_registration.router)
 app.include_router(update_expense.router)
 app.include_router(update_driver.router)
+app.include_router(trip_report.router)
+app.include_router(expense_report.router)
 
 
 @app.get("/")
