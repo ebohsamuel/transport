cc72c4d23cbfee5043f73ec8fd7baecfa856657a
Eboh Samuel <ebohsamuel100@yahoo.com>
2025-01-24 11:31:25 +0100

    updating template files

--- transport_app/database.py
+++ transport_app/database.py
@@ -2,10 +2,10 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sess
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 
-# "postgresql://postgres:PG/eng1102493@localhost:5432/shop_application.db" for local server"
-DATABASE_URL = "sqlite+aiosqlite:///./transport.db"
+# sqlite+aiosqlite:///./transport.db for local server"
+DATABASE_URL = "postgresql+asyncpg://postgres:PG/eng1102493@localhost:5432/transport"
 
-async_engine = create_async_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+async_engine = create_async_engine(DATABASE_URL)
 
 async_SessionLocal = async_sessionmaker(autoflush=False, bind=async_engine, expire_on_commit=False)
 
